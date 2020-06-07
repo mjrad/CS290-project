18d79c2481d1b20845be8377dbd90a7d76d5f4e4
mjrad <299252@bsd48.org>
2020-06-07 13:06:20 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
+var data = require('./data');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.engine('handlebars', exphbs({ defaultLayout: 'basic' }));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
 app.get('', function (req, res, next) {
-  res.status(200).render('partials/main);
+  res.status(200).render('partials/postContainer', data);
 });
 
 
