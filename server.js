28e88b2f23d465edd512ec475cad1edc407e8b95
Akinyemo <53135944+Akinyemo@users.noreply.github.com>
2020-06-08 18:17:37 -0700

    Fixed Post not displaying
    Fixed minor issues

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
 app.get('', function (req, res, next) {
-  res.status(200).render('partials/postContainer', data);
+  res.status(200).render('partials/postContainer', {
+    totalPost : data 
+  });
+  console.log(data);
   //res.status(200).sendFile(__dirname + '/public/main.html');
 });
 
