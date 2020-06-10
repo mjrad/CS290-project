f20f8e78a880967f9f1a4e4d60e1c43dd361ca4e
Bolaji Akinyemi <53135944+Akinyemo@users.noreply.github.com>
2020-06-10 12:22:33 -0700

    Update main.js

--- public/main.js
+++ public/main.js
@@ -46,13 +46,23 @@ function modalAcceptClick() {
       caption: postCaption,
       comments : emptyComments
     });
-    console.log(requestBody);
-    allPosts.push({
-      author: postAuthor,
-      url: postURL,
-      caption: postCaption
-    });
 
+    request.setRequestHeader(
+      'Content-Type',
+      'application/json'
+    );
+
+      request.addEventListener('load',function(event){
+      if(event.target.status === 200){
+        var postTemplate = Handlebars.templates.post;
+        var newPost = postTemplate({
+          author : postAuthor,
+          url : postURL,
+          caption : postCaption
+        });
+      }});
+
+    allPosts.insertAdjacentHTML('beforeend',newPost);
     clearSearch();
     hidePostModal();
   } else {
@@ -262,26 +272,17 @@ window.addEventListener('DOMContentLoaded', function () {
         toggle(event.target)
       });
 
-      console.log(tmp);
-      commentShow.addEventListener('click', function(){
-        console.log("Calling toggleComments with this val :",tmp); 
-        toggleComments(tmp-1);
-      });
+      commentShow.addEventListener('click',toggleComments.bind(tmp));
 
-      addCommentButton.addEventListener('click', function(){
-        showCommentModal();
-      });
+      commentShow.addEventListener('click',showCommentModal.bind(tmp));
       
 
-      deleteButton.addEventListener('click', function(){
-        var tmp = event.target;
-        console.log("event target",event.target);
-        deletePost();
-      });
+      deleteButton.addEventListener('click', deletePost.bind(tmp));
       //jump to the next set of four buttons
       i += 4;
-      tmp ++;
+      
     }
+    tmp++;
   }
 
 });
@@ -298,8 +299,8 @@ function toggle(likeButton) {
   }
 }
 
-function deletePost(){
-  console.log('You are deleting post', postFromButton);
-  console.log(allPosts[postFromButton]);
-  document.removeChild(allPosts[postFromButton]);
+function deletePost(index){
+  console.log('You are deleting post', index);
+  console.log(allPosts[index]);
+  document.removeChild(allPosts[index]);
 }
